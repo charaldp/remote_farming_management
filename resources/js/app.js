c5c906a9cbbfe9f743b9c533e94f7f37572770f8
charaldp <charaldp@gmail.com>
2020-09-08 19:49:51 +0300

    Fix package vulnerability

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
 require('./bootstrap');
 window.Vue = require('vue');
-Vue.component('cylindrical-shell-component-checks', require('./components/cylindrical_shell/cylindricalShellComponentChecks.vue').default);
+// Vue.component('cylindrical-shell-component-checks', require('./components/cylindrical_shell/cylindricalShellComponentChecks.vue').default);
