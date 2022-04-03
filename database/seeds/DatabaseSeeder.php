a720b11c46c6ed184b7fcf38a4b7573ab922c0a6
charaldp <charaldp@gmail.com>
2022-04-03 21:31:51 +0300

    Changes for seeder and env file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ScheduleSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(ScheduleSeeder::class);
     }
 }
